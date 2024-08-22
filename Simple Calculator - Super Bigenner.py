5078b51ef75d12f755a629ecc8cf71e988db314e
Sayed Younas Sadat <130991896+SayedYounasSadat@users.noreply.github.com>
2024-08-22 20:27:41 +0430

    Add files via upload

---
+++ Simple Calculator - Super Bigenner.py
@@ -0,0 +1,15 @@
+print("Write your numbers so my match machine can calculate them for you!")
+num1 = input("Write your first number here: ")
+num2 = input("Write your second number here: ")
+
+sum = int(num1) + int(num2)
+print("the sum of", num1, "with", num2, "is", sum)
+
+difference = int(num1) - int(num2)
+print("the difference netween", num1, "and", num2, "is", difference)
+
+product = int(num1) * int(num2)
+print("the product of", num1, "and", num2, "is", product)
+
+quotient = int(num1) / int(num2)
+print("the quotient of", num1, "and", num2, "is", quotient)
